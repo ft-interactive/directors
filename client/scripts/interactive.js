dd5bfe43444ac5fdb60df14d1d36097e244ffb04
Joanna Kao <joanna.kao@ft.com>
2016-08-10 13:45:08 -0400

    rotate to horizontal + style changes with claire

--- client/scripts/interactive.js
+++ client/scripts/interactive.js
@@ -1,6 +1,12 @@
+// const dataset;
+
+// d3.csv('data/sortabletable.csv', function(row) {
+
+// });
+
 function displayCharts(companyName) {
-	document.getElementById("result-tenure").getElementsByClassName("result-chart-rect--company")[0].style.height = "90px";
-	document.getElementById("result-tenure").getElementsByClassName("result-chart-rect--company")[0].getElementsByClassName("result-chart-val")[0].innerHTML = 8;
+	document.getElementById('result-tenure').getElementsByClassName('result-chart-rect--company')[0].style.width = '90%';
+	document.getElementById('result-tenure').getElementsByClassName('result-chart-rect--company')[0].getElementsByClassName('result-chart-val')[0].innerHTML = 8;
 }
 
-displayCharts("Apple");
+displayCharts('Apple');
